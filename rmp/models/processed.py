b29f18cb503da15dc70750422b5a847edef1b60a
James Gordon <gordon.je@gmail.com>
2018-09-16 22:18:59 -0500

    remove rmp prefix from model names

--- rmp/models/processed.py
+++ rmp/models/processed.py
@@ -4,7 +4,7 @@ Models for processed RMP data.
 from django.db import models
 
 
-class RmpAccChem(models.Model):
+class AccChem(models.Model):
     accchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     accident_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     chemical_id = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -18,7 +18,7 @@ class RmpAccChem(models.Model):
         db_table = 'rmp_acc_chem'
 
 
-class RmpAccFlam(models.Model):
+class AccFlam(models.Model):
     flammixchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     accchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     chemical_id = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -27,7 +27,7 @@ class RmpAccFlam(models.Model):
         db_table = 'rmp_acc_flam'
 
 
-class RmpChemCd(models.Model):
+class ChemCd(models.Model):
     chemical_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     chemical_name = models.CharField(max_length=-1)
     cas2 = models.CharField(max_length=-1, blank=True, null=True)
@@ -40,7 +40,7 @@ class RmpChemCd(models.Model):
         db_table = 'rmp_chem_cd'
 
 
-class RmpDeregCd(models.Model):
+class DeregCd(models.Model):
     dereg = models.DecimalField(max_digits=65535, decimal_places=65535)
     dereg_tr = models.CharField(max_length=-1)
 
@@ -48,7 +48,7 @@ class RmpDeregCd(models.Model):
         db_table = 'rmp_dereg_cd'
 
 
-class RmpDochanCd(models.Model):
+class DochanCd(models.Model):
     dochan = models.CharField(max_length=-1)
     dochan_tr = models.CharField(max_length=-1)
 
@@ -56,7 +56,7 @@ class RmpDochanCd(models.Model):
         db_table = 'rmp_dochan_cd'
 
 
-class RmpDoctypCd(models.Model):
+class DoctypCd(models.Model):
     doctyp = models.CharField(max_length=-1)
     doctyp_tr = models.CharField(max_length=-1)
 
@@ -64,7 +64,7 @@ class RmpDoctypCd(models.Model):
         db_table = 'rmp_doctyp_cd'
 
 
-class RmpEventsCd(models.Model):
+class EventsCd(models.Model):
     events = models.CharField(max_length=-1)
     events_tr = models.CharField(max_length=-1)
 
@@ -72,7 +72,7 @@ class RmpEventsCd(models.Model):
         db_table = 'rmp_events_cd'
 
 
-class RmpExecSumLen(models.Model):
+class ExecSumLen(models.Model):
     rmp_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     byte_count = models.DecimalField(max_digits=65535, decimal_places=65535)
     suspect_count = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -83,7 +83,7 @@ class RmpExecSumLen(models.Model):
         db_table = 'rmp_exec_sum_len'
 
 
-class RmpLldescCd(models.Model):
+class LldescCd(models.Model):
     lldesc = models.CharField(max_length=-1)
     lldesc_tr = models.CharField(max_length=-1)
 
@@ -91,7 +91,7 @@ class RmpLldescCd(models.Model):
         db_table = 'rmp_lldesc_cd'
 
 
-class RmpLlmethCd(models.Model):
+class LlmethCd(models.Model):
     primary_key = models.DecimalField(max_digits=65535, decimal_places=65535)
     llmeth = models.CharField(max_length=-1)
     llmeth_tr = models.CharField(max_length=-1)
@@ -100,7 +100,7 @@ class RmpLlmethCd(models.Model):
         db_table = 'rmp_llmeth_cd'
 
 
-class RmpPhysCd(models.Model):
+class PhysCd(models.Model):
     phys = models.CharField(max_length=-1)
     phys_tr = models.CharField(max_length=-1)
 
@@ -108,7 +108,7 @@ class RmpPhysCd(models.Model):
         db_table = 'rmp_phys_cd'
 
 
-class RmpPrevent2Chem(models.Model):
+class Prevent2Chem(models.Model):
     primary_key = models.DecimalField(max_digits=65535, decimal_places=65535)
     prevent_2_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     procchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -117,7 +117,7 @@ class RmpPrevent2Chem(models.Model):
         db_table = 'rmp_prevent_2_chem'
 
 
-class RmpPrevent3Chem(models.Model):
+class Prevent3Chem(models.Model):
     primary_key = models.DecimalField(max_digits=65535, decimal_places=65535)
     prevent_3_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     procchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -126,7 +126,7 @@ class RmpPrevent3Chem(models.Model):
         db_table = 'rmp_prevent_3_chem'
 
 
-class RmpProcChem(models.Model):
+class ProcChem(models.Model):
     procchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     process_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     chemical_id = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -146,7 +146,7 @@ class RmpProcChem(models.Model):
         db_table = 'rmp_proc_chem'
 
 
-class RmpProcFlam(models.Model):
+class ProcFlam(models.Model):
     flammixchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     procchem_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     chemical_id = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -155,7 +155,7 @@ class RmpProcFlam(models.Model):
         db_table = 'rmp_proc_flam'
 
 
-class RmpProcNaics(models.Model):
+class ProcNaics(models.Model):
     procnaics_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     process_id = models.DecimalField(max_digits=65535, decimal_places=65535)
     naics = models.DecimalField(max_digits=65535, decimal_places=65535)
@@ -166,7 +166,7 @@ class RmpProcNaics(models.Model):
         db_table = 'rmp_proc_naics'
 
 
-class RmpRejectCd(models.Model):
+class RejectCd(models.Model):
     reject = models.CharField(max_length=-1)
     reject_tr = models.CharField(max_length=-1)
 
@@ -174,7 +174,7 @@ class RmpRejectCd(models.Model):
         db_table = 'rmp_reject_cd'
 
 
-class RmpScenCd(models.Model):
+class ScenCd(models.Model):
     scen = models.CharField(max_length=-1)
     scen_tr = models.CharField(max_length=-1)
 
@@ -182,7 +182,7 @@ class RmpScenCd(models.Model):
         db_table = 'rmp_scen_cd'
 
 
-class RmpSubmitCd(models.Model):
+class SubmitCd(models.Model):
     submit = models.CharField(max_length=-1)
     submit_tr = models.CharField(max_length=-1)
 
@@ -190,7 +190,7 @@ class RmpSubmitCd(models.Model):
         db_table = 'rmp_submit_cd'
 
 
-class RmpTopoCd(models.Model):
+class TopoCd(models.Model):
     topo = models.CharField(max_length=-1)
     topo_tr = models.CharField(max_length=-1)
 
@@ -198,7 +198,7 @@ class RmpTopoCd(models.Model):
         db_table = 'rmp_topo_cd'
 
 
-class RmpWindCd(models.Model):
+class WindCd(models.Model):
     wind = models.CharField(max_length=-1)
     wind_tr = models.CharField(max_length=-1)
 
