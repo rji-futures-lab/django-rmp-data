a4897166513beae7c0afda07cc6d1076340799fc
James Gordon <gordon.je@gmail.com>
2019-04-17 21:35:57 -0500

    #84

--- config/settings/local.py
+++ config/settings/local.py
@@ -47,3 +47,19 @@ LOGGING = {
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+# STATIC
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = [
+    os.path.join(ROOT_DIR, 'rmp', 'static'),
+]
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,21 +9,21 @@ ALLOWED_HOSTS = [
     'rmp.rjifuture.org',
 ]
 
-INSTALLED_APPS = INSTALLED_APPS + ['storages', ]
+INSTALLED_APPS = INSTALLED_APPS + ['storages',]
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_REGION_NAME')
+AWS_STORAGE_BUCKET_NAME = 'rtk-static'
+AWS_BUCKET_ACL = 'public-read'
 AWS_DEFAULT_ACL = None
+AWS_AUTO_CREATE_BUCKET = True
+AWS_S3_USE_SSL = False
+AWS_QUERYSTRING_AUTH = False
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# RMP_DATA_DIR = os.path.join(ROOT_DIR, 'data', 'rmp')
-# RMP_RAW_DATA_DIR = os.path.join(RMP_DATA_DIR, 'raw')
-# RMP_PROCESSED_DATA_DIR = os.path.join(RMP_DATA_DIR, 'processed')
